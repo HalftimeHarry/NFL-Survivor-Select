a97a457a7bce3bf0fde52bb3bd624d753692c579
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-06-22 19:56:37 +0000

    Back on track

--- backend/test/PoolMaster.js
+++ backend/test/PoolMaster.js
@@ -1,3 +1,4 @@
+const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
 const NAME = "PoolMaster";
@@ -10,212 +11,60 @@ const POOL_DATE = "Sept 7th";
 const POOL_TIME = "9:00PM EST";
 const WEEK_ID = 1;
 
-describe("PoolMaster", () => {
-  let poolMaster;
-  let entry;
-  let owner, participants;
+describe("PoolMaster and Entry", function () {
+  let PoolMaster, Entry, poolMaster, entry, owner, addr1, addr2;
 
   beforeEach(async () => {
-    try {
-      // Setup accounts
-      [owner, ...participants] = await ethers.getSigners();
+    PoolMaster = await ethers.getContractFactory("PoolMaster");
+    Entry = await ethers.getContractFactory("Entry");
 
-      // Deploy contracts
-      const Entry = await ethers.getContractFactory("Entry");
-      entry = await Entry.deploy({gasLimit: 9500000}); // set a high gas limit
+    [owner, addr1, addr2] = await ethers.getSigners();
 
-      await entry.deployed();
+    entry = await Entry.deploy(owner.address);
+    await entry.deployed();
 
-      const PoolMaster = await ethers.getContractFactory("PoolMaster");
-      poolMaster = await PoolMaster.deploy("PoolMaster", "PM", entry.address, {gasLimit: 9500000}); // set a high gas limit
-      // wait until the contract is definitely deployed
-      await poolMaster.deployed();
-      
-      const poolMasterName = await poolMaster.name();
-      console.log('PoolMaster name:', poolMasterName);
-
-      // List a pool
-      const listTx = await poolMaster.connect(owner).list(
-        WEEK_ID,
-        POOL_NAME,
-        POOL_COST,
-        POOL_MAX_SPOTS,
-        POOL_DATE,
-        POOL_TIME,
-        Math.floor(Date.now() / 1000) + 60, // entry deadline in 60 seconds
-        Math.floor(Date.now() / 1000) + 120 // pick deadline in 120 seconds
-      );
-
-      await listTx.wait();
-    } catch (error) {
-      console.error('Error in beforeEach:', error);
-    }
+    poolMaster = await PoolMaster.deploy("PoolMaster", "PMT", entry.address);
+    await poolMaster.deployed();
   });
 
-  describe("Deployment", () => {
-    it("Sets the name", async () => {
-      const contractName = await poolMaster.name();
-      expect(contractName).to.equal(NAME);
+  describe("Entry contract", function () {
+    it("Should mint a new token", async function () {
+      await entry.mint(addr1.address, "tokenURI1");
+      expect(await entry.totalSupply()).to.equal(1);
     });
+
+    it("Should allow owner to pick a team", async function () {
+      const tokenId = await entry.mint(addr1.address, "tokenURI1");
+      await entry.connect(addr1).pickTeam(tokenId, 1, 2);
+      const [weekIds, teamIds] = await entry.getPickedTeams(tokenId);
+      expect(weekIds[0]).to.equal(1);
+      expect(teamIds[0]).to.equal(2);
+    });
+
+    it("Should not allow non-owner to pick a team", async function () {
+      const tokenId = await entry.mint(addr1.address, "tokenURI1");
+      await expect(
+        entry.connect(addr2).pickTeam(tokenId, 1, 2)
+      ).to.be.revertedWith("Only the entry owner can pick a team");
+    });
+
+    // Add more tests as required
   });
 
-  describe("Pool Listing", () => {
-    it('Lists a pool', async () => {
-      const poolId = 1; // Assuming you are listing the first pool
-
-      // Join the pool before listing it
-      const listPoolTx = await poolMaster.connect(participants[0]).enter(poolId, { value: POOL_COST });
-      await listPoolTx.wait();
-
-      // Retrieve the pool details
-      const listedPool = await poolMaster.getPool(poolId);
-
-      // Assert that the listed pool details match the expected values
-      expect(listedPool.name).to.equal(POOL_NAME);
-      expect(listedPool.cost).to.equal(POOL_COST);
-      expect(listedPool.spots).to.equal(POOL_MAX_SPOTS);
-      expect(listedPool.date).to.equal(POOL_DATE);
-      expect(listedPool.time).to.equal(POOL_TIME);
-      // Add more assertions for other properties if needed
+  describe("PoolMaster contract", function () {
+    it("Should list a new pool", async function () {
+      await poolMaster.connect(owner).list(1, "Pool 1", 1, 10, "2023-09-01", "00:00:00", 1670064000, 1670064100);
+      const pool = await poolMaster.getPool(1);
+      expect(pool.name).to.equal("Pool 1");
+      expect(pool.cost).to.equal(1);
     });
+
+    it("Should allow users to enter a pool", async function () {
+      await poolMaster.connect(owner).list(1, "Pool 1", 1, 10, "2023-09-01", "00:00:00", 1670064000, 1670064100);
+      await poolMaster.connect(addr1).enter(1, { value: ethers.utils.parseEther("1") });
+      expect(await poolMaster.getEntriesCount(1)).to.equal(1);
+    });
+
+    // Add more tests as required
   });
-
-    describe("Pool Entry", () => {
-    it('Allows entry before deadline', async () => {
-      const allowEntryTx = await poolMaster.connect(participants[0]).enter(1, { value: POOL_COST });
-      await allowEntryTx.wait();
-
-      const participant = participants[0].address;
-      const poolId = 1; // Assuming you are entering the first pool
-      
-      const hasEntered = await poolMaster.hasEntered(poolId, participant);
-      console.log('Has Entered:', hasEntered);
-
-      expect(hasEntered).to.equal(true);
-
-      // Verify the updated state variable for number of entries
-      const expectedEntriesCount = 1;
-      const actualEntriesCount = await poolMaster.getEntriesCount(poolId);
-      expect(actualEntriesCount).to.equal(expectedEntriesCount);
-    });
-    });
-  });
- describe("Team Selection", () => {
-  let poolMaster;
-
-  beforeEach(async () => {
-    try {
-      // Setup accounts
-      [owner, ...participants] = await ethers.getSigners();
-
-      // Deploy contracts
-      const Entry = await ethers.getContractFactory("Entry");
-      entry = await Entry.deploy({ gasLimit: 9500000 });
-      console.log('Entry contract deployed at:', entry.address);
-
-      const PoolMaster = await ethers.getContractFactory("PoolMaster");
-      poolMaster = await PoolMaster.deploy("PoolMaster", "PM", entry.address, { gasLimit: 9500000 });
-      console.log('PoolMaster contract deployed at:', poolMaster.address);
-
-      const poolMasterName = await poolMaster.name();
-      console.log('PoolMaster name:', poolMasterName);
-
-      // List a pool
-      const listTx = await poolMaster.connect(owner).list(
-        WEEK_ID,
-        POOL_NAME,
-        POOL_COST,
-        POOL_MAX_SPOTS,
-        POOL_DATE,
-        POOL_TIME,
-        Math.floor(Date.now() / 1000) + 60, // entry deadline in 60 seconds
-        Math.floor(Date.now() / 1000) + 120 // pick deadline in 120 seconds
-      );
-
-      await listTx.wait();
-    } catch (error) {
-      console.error('Error in beforeEach:', error);
-    }
-  });
-        
-   describe("Team Selection", () => {
-    it('Allows team selection before deadline', async () => {
-      const poolId = 1; // Assuming the pool with ID 1 exists
-      const entryId = 1; // Assuming the participant will own an NFT with token ID 1 after entering the pool
-      const selectedTeam = 1; // Assuming you are selecting the first team
-
-      // Enter the pool first to mint an Entry NFT
-      try {
-        const enterPoolTx = await poolMaster.connect(participants[0]).enter(poolId);
-        await enterPoolTx.wait();
-      } catch (error) {
-        console.error('Error in enterPoolTx:', error);
-      }
-
-      // Now, pick a team for the Entry NFT
-      try {
-        const pickTeamTx = await poolMaster.connect(participants[0]).pickTeam(entryId, selectedTeam);
-        await pickTeamTx.wait();
-      } catch (error) {
-        console.error('Error in pickTeamTx:', error);
-      }
-    });
-   });
-   
-    it('Prevents team selection after deadline', async () => {
-      // Set the current time to be after the deadline
-      const currentTimestamp = Math.floor(Date.now() / 1000);
-      const poolId = 1; // Assuming you are selecting the first pool
-      const deadline = await poolMaster.getPickDeadline(poolId);
-      const isAfterDeadline = currentTimestamp > deadline;
-
-      expect(isAfterDeadline).to.equal(false); // Expecting false, as team selection is not allowed after the deadline
-    });
-   
-describe("pickTeam", function() {
-  it("Should allow the owner of an entry to pick a team", async function () {
-    const participant = participants[0];
-
-    // Mint an NFT to participant
-    await entry.connect(owner).mint(participant.address, 1);
-
-    // Assume the NFT has token ID 1 and the team ID is also 1
-    const teamId = 1;
-
-    try {
-      // participant picks a team
-      await entry.connect(participant).pickTeam(1, teamId);
-      await entry.getPickedTeams(1);
-
-      let pickedTeams = await entry.getPickedTeams(1);
-      pickedTeams = pickedTeams.map(team => team.toNumber()); // Converts each BigNumber to a regular number
-      // Verify that the team has been picked
-      expect(pickedTeams).to.include(teamId);
-  } catch (error) {
-  console.error('Error message:', error.message);
-  console.error('Error in pickTeamTx:', error);
-  // If there's an error, fail the test
-  expect.fail("Unexpected error during pickTeam execution");
-}
-});
-
-  it("Should not allow a non-owner to pick a team", async function() {
-    // This is the participant that does not own the entry
-    const nonOwner = participants[1];
-
-    // Assume the NFT has token ID 1 and the team ID is also 1
-    const entryId = 1;
-    const teamId = 1;
-
-    try {
-      // Try to have the non-owner pick a team
-      await entry.connect(nonOwner).pickTeam(entryId, teamId);
-
-      // If the transaction didn't fail, then fail the test
-      expect.fail("Expected an error, but did not get one");
-    } catch (error) {
-      // Expected an error, so the test passes
-    }
-  });
- });
 });
